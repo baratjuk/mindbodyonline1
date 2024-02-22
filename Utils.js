6ff750ec9440f6401f15c133b5349eaa6c736dc5
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-02-22 17:56:47 +0200

    utils

--- Utils.js
+++ Utils.js
@@ -16,45 +16,50 @@ class Utils {
 
     print_object(obj) {
         return `\n${this.print(obj, 0, false)}`
-    }  
-    
+    }
+
     print_object_and_func(obj) {
         return `\n${this.print(obj, 0, true)}`
     }
 
     print(printed_object, level, is_func) {
-        if(typeof printed_object == 'object') {
-            let ret_str = ''
-            for(let key in printed_object) {
-                let str = ''
-                for(let i = 0; i<level; i++) {
-                    str += '  '    
-                }
-                str += `${key} : ` 
-                let obj = printed_object[key]
-                let type = typeof obj
-                if (type == 'boolean' || type == 'number' || type == 'string') {
-                    str += `${obj}\n`
-                } else if (type == 'object') {
-                    if(level < 3) {
-                        str += `\n${this.print(obj, level + 1)}`
-                    } else {
-                        str += '...\n'
+        try {
+            if (typeof printed_object == 'object') {
+                let ret_str = ''
+                for (let key in printed_object) {
+                    let str = ''
+                    for (let i = 0; i < level; i++) {
+                        str += '  '
                     }
-                } else if (type == 'function') {
-                    if(is_func) {
+                    str += `${key} : `
+                    let obj = printed_object[key]
+                    let type = typeof obj
+                    if (type == 'boolean' || type == 'number' || type == 'string') {
                         str += `${obj}\n`
+                    } else if (type == 'object') {
+                        if (level < 9) {
+                            str += `\n${this.print(obj, level + 1)}`
+                        } else {
+                            str += '...\n'
+                        }
+                    } else if (type == 'function') {
+                        if (is_func) {
+                            str += `${obj}\n`
+                        } else {
+                            str += `${type}\n`
+                        }
                     } else {
                         str += `${type}\n`
                     }
-                } else {
-                    str += `${type}\n`
+                    ret_str += str
                 }
-                ret_str += str
+                return ret_str
+            } else {
+                return 'not an object'
             }
-            return ret_str
-        } else {
-            return 'not an object'
+        } catch (e) {
+            this.log('parse error : ' + e.stack)
+            return 'parse error : ' + e.message
         }
     }
 
