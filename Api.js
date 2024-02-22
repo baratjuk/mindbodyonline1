1d6f321c46c7bc4db3b170dac9f39e98febf01d6
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-02-22 08:23:32 +0200

    patch

--- Api.js
+++ Api.js
@@ -3,6 +3,18 @@ import Utils from './Utils.js';
 
 class Api {
     static API_KEY = '006b55a0c1904396a8815b33a52063bd'
+    static EVENTS = [
+        'client.created',
+        'client.updated',
+        'client.deactivated',
+        'clientProfileMerger.created',
+        'clientMembershipAssignment.created',
+        'clientMembershipAssignment.cancelled',
+        'clientContract.created',
+        'clientContract.updated',
+        'clientContract.cancelled',
+        'clientSale.created',
+      ]
     static TIMEOUT = 5000
     utils = new Utils('api_test1.log')
 
@@ -18,28 +30,22 @@ class Api {
                 }
             }
         )
-        this.utils.log('subscribe url : ' + url + ' => ' + response.status)
+        this.utils.log('subscriptions url : ' + url + ' => ' + response.status)
         if (response.status === 200) {
             let data = response.data
-            this.utils.log('subscribe data : ' + this.utils.print_object(data))
+            this.utils.log('subscriptions data : ' + this.utils.print_object(data))
             return data
         }    
         return {}
     }
-    
-    async subscribe(query) {
-        let {event} = query
-        if(!event) {
-            return {}
-        }
+
+    async subscribeAll() {
         let url = `https://mb-api.mindbodyonline.com/push/api/v1/subscriptions`
         let content = {
-            eventIds: [
-                event
-              ],
+            eventIds: Api.EVENTS,
               eventSchemaVersion: 1,
               webhookUrl: 'https://dev1.htt.ai/',
-              referenceId: `test1_${event}`
+              referenceId: `test1`
         }
         try {
             let response = await axios.post(
@@ -72,12 +78,11 @@ class Api {
         }
         let url = `https://mb-api.mindbodyonline.com/push/api/v1/subscriptions/${subscriptionId}`
         let content = {
-            eventIds: [
-                event
-              ],
+            eventIds: Api.EVENTS,
               eventSchemaVersion: 1,
               webhookUrl: 'https://dev1.htt.ai/',
-              referenceId: `test1_${event}`
+              referenceId: `test1`,
+              status: 'Active'
         }
         try {
             let response = await axios.patch(
@@ -91,14 +96,14 @@ class Api {
                     }
                 }
             )
-            this.utils.log('subscribe url : ' + url + ' => ' + response.status)
-            if (response.status === 200) {
+            this.utils.log('patch url : ' + url + ' => ' + response.status)
+            if (response.status < 300) {
                 let data = response.data
-                this.utils.log('subscribe data : ' + this.utils.print_object(data))
+                this.utils.log('patch data : ' + this.utils.print_object(data))
                 return data
             }  
         } catch(e) {
-            this.utils.log('subscribe error : ' + e.message)   
+            this.utils.log('patch error : ' + e.message)   
             throw e     
         }  
     }
@@ -122,11 +127,11 @@ class Api {
             this.utils.log('delete url : ' + url + ' => ' + response.status)
             if (response.status === 200) {
                 let data = response.data
-                this.utils.log('subscribe data : ' + this.utils.print_object(data))
+                this.utils.log('delete data : ' + this.utils.print_object(data))
                 return data
             }  
         } catch(e) {
-            this.utils.log('subscribe error : ' + e.message)      
+            this.utils.log('delete error : ' + e.message)      
             throw e 
         }  
         return {}
--- app.js
+++ app.js
@@ -16,14 +16,12 @@ const server = http.createServer((req, res) => {
 })
 server.listen(port)
 
-let options = {
-    key: fs.readFileSync(`/opt/mindbody/mindbodyonline1/key.pem`),
-    cert: fs.readFileSync(`/opt/mindbody/mindbodyonline1/cert.pem`),
-    passphrase: 'quake2'
-}
-
+// let options = {
+//     key: fs.readFileSync(`/opt/mindbody/mindbodyonline1/key.pem`),
+//     cert: fs.readFileSync(`/opt/mindbody/mindbodyonline1/cert.pem`),
+//     passphrase: 'quake2'
+// }
 // console.log(utils.print_object(options))
-
 // const httpsServer = https.createServer(options, (req, res) => {
 //     serverRequest(req, res)
 // })
@@ -49,24 +47,32 @@ const serverRequest = async (req, res) => {
         let query = parts.query
 
         utils.log('GET : ' + req.url)
+
         switch (parts.pathname) {
-            case '/subscriptions':
-                let json = await api.subscriptions()
-                insert(req.url, 'GET', json)
-                break
-            case '/subscribe':
-                utils.log('subscribe : ' + utils.print_object(query))
-                if (query) {
-                    let json = await api.subscribe(query)
+            case '/subscriptions': { // https://dev1.htt.ai/subscriptions
+                    let json = await api.subscriptions()
                     insert(req.url, 'GET', json)
                 }
                 break
-            case '/delete':
-                let {id} = query
-                utils.log('delete : ' + utils.print_object(query))
-                if (id) {
-                    let json = await api.delete(id)
+            case '/subscribe': { // https://dev1.htt.ai/subscribe
+                    let json = await api.subscribeAll()
                     insert(req.url, 'GET', json)
+                }
+                break
+            case '/patch': { // https://dev1.htt.ai/patch?id=07ac8fee-21c6-4363-96fc-b0e3178b88bc
+                    let { id } = query
+                    if (id) {
+                        let json = await api.patchSubscribe(id)
+                        insert(req.url, 'GET', json)
+                    }
+                }
+                break    
+            case '/delete': {// https://dev1.htt.ai/delete?id=07ac8fee-21c6-4363-96fc-b0e3178b88bc
+                    let { id } = query
+                    if (id) {
+                        let json = await api.delete(id)
+                        insert(req.url, 'GET', json)
+                    }
                 }
                 break
             case '/favicon.ico':
