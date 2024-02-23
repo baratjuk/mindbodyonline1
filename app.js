9b6729f92fac3db2464c9d82a5429ad8be430f2d
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-02-23 19:21:09 +0200

    fixed bugs

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ const serverRequest = async (req, res) => {
             case '/favicon.ico':
                 break        
             default:
-                db.insertWebhook(req.method, req.url, {}, {})
+                db.insertWebhook(req.method, req.url, {}, req.headers)
         }
         res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' })
         res.write('GET OK')
