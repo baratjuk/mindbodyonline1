946ac62dc9ef21b8176019fe43c4e8aacebab6e7
Blueforce Dev <blueforcedev@staturesoftware.com>
2024-03-27 11:33:45 +0200

    fixed bug

--- Db.js
+++ Db.js
@@ -121,7 +121,7 @@ class Db {
         this.dbConnect.query(sql, (err, result) => {
             if (err && !isError) {
                 this.utils.log('insertClient error : ' + err.message)
-                this.insertClient({error: err.message, Id: data.Id}, true)
+                this.insertClient({error: 1, Id: data.Id}, true)
             }
             if (err && isError) {
                 this.utils.log('insertClient error ID : ' + data.Id + ' ' + err.message)
